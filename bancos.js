0312188889e0db8affca2859c0e9bb01fccd4522
Christian Angelone <christiangelone@gmail.com>
2019-08-31 13:58:21 -0300

    Improves bank listing

--- bancos.js
+++ bancos.js
@@ -1,31 +1,7 @@
-module.exports = [
-    "BANCO DE LA NACION ARGENTINA",
-    "BANCO DE CORRIENTES S.A.",
-    "BANCO DE FORMOSA S.A.",
-    "BANCO DEL CHUBUT S.A.",
-    "BANCO DE LA CIUDAD DE BUENOS AIRES",
-    "BANCO DE LA PROVINCIA DE BUENOS AIRES",
-    "BANCO PCIA. DE TIERRA DEL FUEGO",
-    "BANCO DE SANTIAGO DEL ESTERO S.A.",
-    "BANCO FINANSUR S.A.",
-    "Banco Columbia",
-    "Banco Comafi",
-    "Banco del Sol",
-    "Banco Galicia",
-    "Banco Itaú",
-    "Banco Macro",
-    "Banco Patagonia",
-    "BANCO PIANO S.A.",
-    "BANCO HIPOTECARIO S.A.",
-    "BBVA Banco Francés",
-    "Banco Santander Río",
-    "Banco Supervielle",
-    "CABAL COOP. LTDA.",
-    "CitiBank",
-    "Compania Financiera",
-    "HSBC Bank Argentina",
-    "ICBC",
-    "NUEVO BANCO DEL CHACO S.A.",
-    "NUEVO BANCO DE SANTA FE S.A.",
-    "NUEVO BCO. INDUSTRIAL DE AZUL S.A."
-]
+const atms = require('./atms')
+const distinct = (value, index, self) => self.indexOf(value) === index
+
+module.exports = atms
+    .map(atm => atm.banco)
+    .filter(distinct)
+    .sort()
